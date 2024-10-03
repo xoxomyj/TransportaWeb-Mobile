e05c575d8249efd8b9c4de06aea98e1d37118501
xoxomyj <marindigos@icloud.com>
2024-10-03 16:39:08 -0300

    inicial feita

---
+++ app/src/main/java/br/senai/sp/jandira/transportaweb/screens/gerais/Inicial.kt
@@ -0,0 +1,95 @@
+package br.senai.sp.jandira.transportaweb.screens.gerais
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.transportaweb.R
+
+@Composable
+fun WelcomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logoo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(325.dp)
+            )
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Text(
+                text = "- SEJA BEM VINDO! -",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .width(327.dp)
+                    .height(48.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFC00000))
+            ) {
+                Text(
+                    text = "ENTRAR",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .width(327.dp)
+                    .height(48.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFC00000))
+            ) {
+                Text(
+                    text = "CADASTRAR-SE",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreview() {
+    MaterialTheme {
+        WelcomeScreen()
+    }
+}
